1ca6072c6b725401ce5d9e9abc0f9ba14953a8e8
dieromeo <diegoromeo99@icloud.com>
2023-11-15 18:38:57 +0100

    feat: miglioramenti e commenti

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 // Function 
 
-function myCreateDomElement(tag, className, content) {
+function myCreateDomElement(tag, className, content) { // funzione che crea un elemento html con contenuto
 
     const newElement = document.createElement(tag);
     newElement.classList.add(className);
@@ -11,24 +11,23 @@ function myCreateDomElement(tag, className, content) {
     return newElement;
 }
 
-function createNcells(htmlElement, num, className) {
+function createNcells(htmlElement, num, className) { // funzione che crea n elementi cella e li appende a un elemento dato come valore di input
 
     for (let i = 1; i <= num; i++) {
         const cell = myCreateDomElement('div', className, i);
         htmlElement.append(cell);
-        cell.addEventListener('click', function () {
+        cell.addEventListener('click', function () { // aggiunge evento click sulle celle
             clicked(cell, 'yellow');
         })
     }
 }
 
-function clicked(htmlElement, tag) {
-
+function clicked(htmlElement, tag) {  // funzione che aggiunge una classe e stampa il contenuto di un elemento
     htmlElement.classList.add(tag);
     console.log(htmlElement.innerHTML);
 }
 
-function createByDifficulty(difficulty) {
+function createByDifficulty(difficulty) {  // funzione che richiama ceateNcells e a seconda della difficoltà inserità ne stampa un numero determinato
     if (difficulty === 'Easy') {
         createNcells(board, 100, 'cell-10');
     }
@@ -43,24 +42,22 @@ function createByDifficulty(difficulty) {
 
 // end function
 
-const startButton = document.getElementById('start');
-const boardContainer = document.querySelector('.board-container');
-console.log(startButton, boardContainer);
-let state = true;
+const startButton = document.getElementById('start'); // variabile bottone
+const boardContainer = document.querySelector('.board-container'); // variabile board container
+let state = true; // variabile di stato
 let board;
-const infoUtente = document.getElementById('info');
+const infoUtente = document.getElementById('info'); // h2 da nascondere al click
 
 
 startButton.addEventListener('click', function () {
-    if (!state) {
+    if (!state) { // se è falso rimuove gli elementi del dom e lo imposta a vero per un reset 
         board.remove();
         state = true;
     }
 
-    if (state) {
-        const select = document.getElementById('difficulty').value;
-        console.log(select);
+    if (state) { // crea e aggiunge gli elementi al dom e imposta state a false per un successivo click
 
+        const select = document.getElementById('difficulty').value;
         board = myCreateDomElement('div', 'board', '')
         boardContainer.append(board);
         createByDifficulty(select);
